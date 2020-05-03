007d4e9d962b40f1b83b9ba6ce7f542587a67de2
Jamie Fernandez <jamie.fernandez@imtins.com>
2020-05-03 12:48:19 -0500

    Initial commit

---
+++ src/components/Board.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="game-board">
+        <div class="box">X</div>
+        <div class="box">X</div>
+        <div class="box">X</div>
+        <div class="box">X</div>
+        <div class="box">X</div>
+        <div class="box">X</div>
+        <div class="box">X</div>
+        <div class="box">X</div>
+        <div class="box">X</div>
+    </div>
+</template>
+<style scoped>
+.game-board 
+{
+    display: grid;
+    grid-template: repeat(3, 1fr) / repeat(3, 1fr);
+}
+</style>
