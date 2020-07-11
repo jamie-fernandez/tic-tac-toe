1f1270bcd735d1750bf6a944c2b4ebeebcfe287a
Jamie Fernandez <jamie.fernandez@imtins.com>
2020-07-11 14:47:43 -0500

    Actually adds view file

---
+++ src/views/tic-tac-toe.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+      <p class="title">Tic Tac Toe</p>
+      <SettingsPrompt/>
+      <Board/>
+    </div>
+</template>
+
+<script>
+import Board from '@/components/Board.vue'
+import SettingsPrompt from '@/components/SettingsPrompt.vue'
+export default {
+    name: 'tic-tac-toe',
+    components: {
+        Board,
+        SettingsPrompt,
+    },
+}
+</script>
