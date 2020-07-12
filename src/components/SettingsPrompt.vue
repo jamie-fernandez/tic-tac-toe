bfb30a3a7b3bde8387d3b0b33933901efdcf2ffd
Jamie Fernandez <jamie.fernandez@imtins.com>
2020-07-12 13:44:45 -0500

    Adds form options in the systems prompt

--- src/components/SettingsPrompt.vue
+++ src/components/SettingsPrompt.vue
@@ -6,7 +6,30 @@
         ok-only
         @ok="startGame"
     >
-        <p class="my-4">Hello from modal!</p>
+      <form ref="form" @submit.stop.prevent="handleSubmit">
+        <b-form-group
+          label="Choose Opponent"
+          label-for="opponent-input"
+        >
+            <b-form-radio-group
+                id="opponent-radio-group"
+                v-model="selectedOpponent"
+                name="opponent-radio-options"
+                :options="optionsOpponent"
+            />
+        </b-form-group>
+        <b-form-group
+          label="X or O"
+          label-for="symbol-input"
+        >
+            <b-form-radio-group
+                id="symbol-radio-group"
+                v-model="selectSymbol"
+                name="symbol-radio-options"
+                :options="optionsSymbol"
+            />
+        </b-form-group>
+      </form>
     </b-modal>
 </template>
 
@@ -15,7 +38,22 @@ import { mapMutations } from 'vuex';
 
 export default {
     name: 'SettingsPrompt',
+    data() {
+      return {
+        selectedOpponent: 'human',
+        optionsOpponent: [
+          { text: 'Human', value: 'human' },
+          { text: 'Computer', value: 'computer' },
+        ],
+        selectSymbol: 'X',
+        optionsSymbol: [
+          { text: 'X', value: 'X' },
+          { text: 'O', value: 'O' },
+        ]
+      }
+    },
     methods: {
+        handleSubmit() {},
         startGame() {
             this.SET_GAME_ACTIVE(true);
         },
